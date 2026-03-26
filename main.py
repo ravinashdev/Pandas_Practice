68aaaf61e1e646a8baad392d333cf85cae6a5620
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-25 21:55:21 -0400

    install pandas

---
+++ main.py
@@ -0,0 +1,20 @@
+# IMPORTS
+import csv
+import itertools
+import pandas
+
+# with open("Weather_Data.csv") as data:
+#     # Creates an iterable object
+#     data = csv.reader(data)
+#     days_of_the_week = []
+#     temperature = []
+#     conditions = []
+#     # Using itertools to slice an iterable object since it has no index to remove csv column labels
+#     for row in itertools.islice(data, 1, None):
+#         days_of_the_week.append(row[0])
+#         temperature.append(int(row[1]))
+#         conditions.append(row[2])
+#     print(days_of_the_week)
+#     print(temperature)
+#     print(conditions)
+
